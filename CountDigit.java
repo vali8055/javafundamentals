edda561e40044cf4895ac7cda09989d8d4da4e88
vali8055 <91729905+vali8055@users.noreply.github.com>
2021-10-10 16:55:19 +0530

    Add files via upload

---
+++ CountDigit.java
@@ -0,0 +1,21 @@
+
+package demo;
+import java.util.Scanner;
+public class CountDigit{
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        sc.close();
+        int i = 0;
+        while(num!=0){
+            num = num/10;
+            i++;
+            if(num == 1){
+                i++;
+                break;
+            }
+        }
+        System.out.println("Number of Digits is : "+ i);
+    }
+}
---
+++ greatestnum.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class greatestnum {
+	
+	    public static void main(String[] args) {
+	        int a = 10, b = 20, c = 30;
+	    if(a > b && a > c){
+	        System.out.println("A is Greatest");
+	    }
+	    else if(b > a && b > c){
+	        System.out.println("B is Greatest");
+	    }
+	    else{
+	        System.out.println("C is Greatest");
+	    }    
+	    }
+	}
+
+
